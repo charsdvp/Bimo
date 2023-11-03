f464376c777cf664438fd9265222d7144fa7651e
charsdvp <carlossanchezdvp@gmail.com>
2023-11-03 14:04:16 -0600

    fix: change url github stars

--- src/components/page/Header.jsx
+++ src/components/page/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <header className="header">
       <nav className="header-nav">
         <h1>Bimo</h1>
-        <a href='https://github.com/charsdvp/chroma-code' target='_blank' className='stars' rel="noreferrer" style={{ textDecoration: 'none' }}><Star /> Github</a>
+        <a href='https://github.com/charsdvp/Bimo/tree/main' target='_blank' className='stars' rel="noreferrer" style={{ textDecoration: 'none' }}><Star /> Github</a>
       </nav>
     </header>
   )
