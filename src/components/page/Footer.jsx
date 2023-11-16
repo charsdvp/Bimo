94c8653b0a08707438087702d40e5ae26d2b7dc0
charsdvp <carlossanchezdvp@gmail.com>
2023-11-16 14:19:01 -0600

    chore(footer): change the structure of the <Footer>

--- src/components/page/Footer.jsx
+++ src/components/page/Footer.jsx
@@ -1,7 +1,18 @@
 export const Footer = () => {
   return (
     <footer className= "footer">
-      <small> &copy; Error 420</small>
+      <small>&copy; Error420</small>
+      <ul className='footer-list'>
+        <li className='footer-list-item'>
+          <a href='https://www.facebook.com/profile.php?id=100087881793975' className='list-item-link'>Facebook</a>
+        </li>
+        <li className='footer-list-item'>
+          <a href='https://twitter.com/charsdvp' className='list-item-link'>Twitter</a>
+        </li>
+        <li className='footer-list-item'>
+          <a href='https://www.instagram.com/_error_cuatro20_/' className='list-item-link'>Instagram</a>
+        </li>
+      </ul>
     </footer>
   )
 }
